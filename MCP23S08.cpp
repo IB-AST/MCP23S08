7820cd9e01a551ea3a888df258da5e5a2ca8e1e6
IB-AST <148654555+IB-AST@users.noreply.github.com>
2025-08-04 12:45:52 +0200

    TEST enable dauerhaft AN

--- MCP23S08.cpp
+++ MCP23S08.cpp
@@ -30,7 +30,7 @@ void MCP23S08::begin() {
 	setPinModes(0x01);
 
     // Set OLAT to 0 (LOW on GP0) — optional, default is 0 anyway
-	setOutputStates(0x00);
+	setOutputStates(0xFF);    // ###TEST###
 
 }
 
